62ed9904b87ebc2ef982c98c449bc42dbc9eaa81
ron8520 <ronming@outlook.com>
2023-05-10 01:06:55 +1000

    init app bar and bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:happy_second/routes/account.dart';
+import 'package:happy_second/routes/cart.dart';
 import 'package:happy_second/routes/home.dart';
+import 'package:happy_second/routes/search.dart';
+import 'package:happy_second/utils/hexColor.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,45 +15,79 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-          backgroundColor: Colors.white,
-          appBar: AppBar(
-              leading: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text("Home Logo")
-                ],
-              ),
-              actions: const <Widget>[
-                Padding(
-                    padding: EdgeInsets.fromLTRB(0, 0, 10, 0), child: CircleAvatar())
-              ]),
-          body: const HomePage(),
-          bottomNavigationBar: BottomNavigationBar(
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home, color: Colors.green),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search, color: Colors.green),
-                label: 'Search',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.shopping_cart, color: Colors.green),
-                label: 'Cart',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person, color: Colors.green),
-                label: 'Account',
-              ),
-            ],
-          )),
-    );
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: MainPage());
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int index = 0;
+  final _pageRouting = const <Widget>[
+    HomePage(),
+    SearchPage(),
+    CartPage(),
+    AccountPage()
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+            centerTitle: true,
+            title: const Image(
+                width: 70,
+                height: 70,
+                image: AssetImage("lib/assets/logo.jpg")),
+            backgroundColor: Colors.white,
+            actions: const <Widget>[
+              Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                  child: CircleAvatar(
+                    child: Icon(Icons.person, color: Colors.white),
+                    backgroundColor: Colors.grey,
+                  ))
+            ]),
+        body: _pageRouting[index],
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: HexColor.fromHex("#5E7737"),
+          unselectedItemColor: Colors.grey[400],
+          currentIndex: index,
+          onTap: (x) {
+            setState(() {
+              index = x;
+            });
+          },
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart),
+              label: 'Cart',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Account',
+            ),
+          ],
+        ));
   }
 }
